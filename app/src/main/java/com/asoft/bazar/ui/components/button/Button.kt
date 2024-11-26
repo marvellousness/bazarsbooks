c9837866c401be67a0909fc772c3c9689009dad7
Tung Phan <tungphan@asnet.com.vn>
2024-11-26 16:55:57 +0700

    Build Buttons component

--- app/src/main/java/com/asoft/bazar/ui/components/button/Button.kt
+++ app/src/main/java/com/asoft/bazar/ui/components/button/Button.kt
@@ -1,99 +1,202 @@
 package com.asoft.bazar.ui.components.button
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ButtonElevation
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.asoft.bazar.ui.components.icon.BazarIcon
+import com.asoft.bazar.R
 import com.asoft.bazar.ui.theme.BazarTheme
-
+import com.asoft.bazar.ui.theme.annotation.LightDarkPreview
 
 @Composable
-internal fun BaseBazarButton(
+fun PrimaryButton(
+    text: String,
     onClick: () -> Unit,
-    colors: ButtonColors = ButtonDefaults.buttonColors(),
-    modifier: Modifier,
-    size: ButtonSize = ButtonSize.Medium,
-    enabled: Boolean = true,
-    elevation: ButtonElevation? = ButtonDefaults.buttonElevation(),
+    modifier: Modifier = Modifier,
+    icon: Painter? = null,
     shape: ButtonShape = ButtonShape.Rounded,
-    border: BorderStroke? = null,
-    icon: BazarIcon? = null,
-    contentPadding: PaddingValues = PaddingValues(
-        horizontal = 16.dp,
-        vertical = size.contentVerticalPadding
-    ),
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    content: @Composable RowScope.() -> Unit,
 ) {
     Button(
         onClick = onClick,
-        modifier = modifier
-            .defaultMinSize(minHeight = size.height),
-        enabled = enabled,
-        elevation = elevation,
-        shape = shape.shape,
-        border = border,
-        colors = colors,
-        interactionSource = interactionSource,
-        contentPadding = contentPadding,
-    ) {
-        Row(
-            modifier = Modifier.weight(weight = 1f, fill = false),
-            content = content
-        )
-    }
-}
-
-@Composable
-fun BazarButton(
-    modifier: Modifier = Modifier,
-    text: String,
-    onClick: () -> Unit,
-    colors: ButtonColors = ButtonDefaults.buttonColors(),
-    size: ButtonSize = ButtonSize.Medium,
-    shape: ButtonShape = ButtonShape.Rounded,
-    enabled: Boolean = true,
-    elevation: ButtonElevation? = ButtonDefaults.buttonElevation(),
-    border: BorderStroke? = null,
-    icon: BazarIcon? = null,
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-) {
-    BaseBazarButton(
-        onClick = onClick,
         modifier = modifier,
-        size = size,
-        shape = shape,
-        enabled = enabled,
-        elevation = elevation,
-        border = border,
-        colors = colors,
-        icon = icon,
-        interactionSource = interactionSource
+        shape = shape.shape
     ) {
+        icon?.let { painter ->
+            Icon(
+                painter = painter,
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+        }
         Text(text = text)
     }
 }
 
-@Preview
 @Composable
-private fun PrimaryButtonPreview() {
+fun SecondaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    icon: Painter? = null,
+    shape: ButtonShape = ButtonShape.Rounded,
+) {
+    FilledTonalButton(
+        onClick = onClick,
+        modifier = modifier,
+        shape = shape.shape,
+    ) {
+        icon?.let { painter ->
+            Icon(
+                painter = painter,
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+        }
+        Text(text = text)
+    }
+}
+
+@Composable
+fun TextButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    icon: Painter? = null,
+) {
+    TextButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        icon?.let { painter ->
+            Icon(
+                painter = painter,
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+        }
+        Text(text = text)
+    }
+}
+
+@Composable
+fun OutlinedButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit,
+    icon: Painter? = null,
+) {
+    OutlinedButton(
+        onClick = onClick,
+        modifier = modifier,
+    ) {
+        icon?.let { painter ->
+            Icon(
+                painter = painter,
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+        }
+        Text(text = text)
+    }
+}
+
+@Composable
+fun SignInButton(
+    text: String,
+    painter: Painter,
+    onClick: () -> Unit,
+) {
+    Surface(
+        modifier = Modifier.clickable(onClick = onClick),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray),
+        color = BazarTheme.colors.surface,
+        shape = ButtonShape.Rounded.shape,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(
+                    start = 12.dp,
+                    end = 16.dp,
+                    top = 12.dp,
+                    bottom = 12.dp
+                )
+                .fillMaxWidth()
+        ) {
+            Icon(
+                painter = painter,
+                contentDescription = "Google Login",
+                tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = text)
+
+            Spacer(modifier = Modifier.width(16.dp))
+        }
+    }
+}
+
+@Composable
+@LightDarkPreview
+private fun ButtonPreview() {
     BazarTheme {
-        BazarButton(
-            text = "Button Button",
-            onClick = {}
-        )
+        Column(
+            modifier = Modifier
+                .background(BazarTheme.colors.background)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            PrimaryButton(
+                text = "Primary Button",
+                icon = painterResource(R.drawable.ic_apple_original),
+                onClick = {}
+            )
+
+            SecondaryButton(
+                text = "Secondary Button",
+                icon = painterResource(R.drawable.ic_apple_original),
+                onClick = {}
+            )
+
+            TextButton(
+                text = "Text Button",
+                icon = painterResource(R.drawable.ic_apple_original),
+                onClick = {}
+            )
+
+            OutlinedButton(
+                text = "Sign in with Google",
+                icon = painterResource(R.drawable.ic_google_original),
+                onClick = {}
+            )
+
+            SignInButton(
+                text = "Sign in With Google",
+                painter = painterResource(id = R.drawable.ic_google_original),
+            ) { }
+        }
     }
 }
---
+++ app/src/main/java/com/asoft/bazar/ui/theme/annotation/LightDarkPreview.kt
@@ -0,0 +1,8 @@
+package com.asoft.bazar.ui.theme.annotation
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+annotation class LightDarkPreview
